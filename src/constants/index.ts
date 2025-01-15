1acd550bfbdac0ebbe834e9dada296629f93d6d6
Eluwing <abek0403@gmail.com>
2025-01-15 23:30:03 +0900

    update

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,7 +4,7 @@ export type SliderItemType = {
 };
 
 export const sliderItemData: SliderItemType[] = [
-    { name: "Slider 1", src: "/parallax.jpg" },
-    { name: "Slider 2", src: "/parallax2.jpg" },
-    { name: "Slider 3", src: "/parallax3.jpg" },
+    { name: "Slider 1", src: "/react-animation-test/parallax.jpg" },
+    { name: "Slider 2", src: "/react-animation-test/parallax2.jpg" },
+    { name: "Slider 3", src: "/react-animation-test/parallax3.jpg" },
   ];
